64ebe034325155ccc33ce5a07d822edcc1bf138d
builder-G <garychui16@gmail.com>
2025-03-11 16:58:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      // Typically, keep Node integration off for security
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  // Load your HTML file
+  win.loadFile('flashcard.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
